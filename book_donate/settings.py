2977da7253f633e634bd98a128c2bb7a1e5983c1
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2024-01-17 17:32:18 +0600

    csrf

--- book_donate/settings.py
+++ book_donate/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-8_4-19yt&qmb%!15+t^798oo7__62s$2n58vv-$okif$q!!l_(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://book-donate-platform.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
